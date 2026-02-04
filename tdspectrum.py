8a2bafde0c6f62513b3ee7cca38882d1d0c1f94f
Cris :) <cristinala2001@gmail.com>
2026-02-04 12:16:45 +0100

    function export svg added

--- tdspectrum.py
+++ tdspectrum.py
@@ -627,3 +627,33 @@ class TDSpectrum:
                     self.absorb[i, :] = self._median_filter1d(self.absorb[i, :], size)
             case _:
                 raise ValueError(f'Unknown smoothing method: {method}')
+    
+    #new function to export plots in svg
+    def export(self, fig, buffer):
+        """
+        Save a plot (Bokeh or Plotly) to a file with high quality.
+        """
+    
+        # --- PLOTLY ---
+        try:
+            if isinstance(fig, go.Figure):
+                fig.write_image(buffer, format="svg")
+                return
+            
+        except ImportError:
+            pass
+    
+        # --- BOKEH ---
+        try:
+            if isinstance(fig, bokeh.plotting.Figure):
+                fig.output_backend = "svg"
+                bkexport.export_svg(fig, filename=buffer)
+                return
+        except ImportError:
+            pass
+    
+        # If it is not Plotly or Bokeh
+        raise ValueError("Unknown figure type. Cannot export.")
+    
+                
+
